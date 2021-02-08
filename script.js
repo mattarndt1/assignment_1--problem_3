e203b14424279282c05339227a21aebd5aba6373
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-07 21:45:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var map = L.map('map').setView([42.35, -71.08], 13);
 
-var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 18,
+	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
 
-$.getJSON('https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/rodents.geojson', function(data) {
+$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){
   var coordinatesOnly = data.features.map(function(feature) {
     return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
   });
